dec0ff2e0b910aa99c3a743fddf03fe3f439a4f9
Mike <partridgems@gmail.com>
2015-09-23 17:58:10 -0400

    classifier written, testing

--- naive_bayes.py
+++ naive_bayes.py
@@ -2,9 +2,11 @@
 
 from classifier import Classifier
 from collections import defaultdict
+import math
+
+smoothing_factor = 0.5
 
 class NaiveBayes(Classifier):
-	smoothing_factor = 0.5
     u"""A naïve Bayes classifier."""
 
     def __init__(self, model={}):
@@ -15,6 +17,7 @@ class NaiveBayes(Classifier):
     model = property(get_model, set_model)
 
     def train(self, instances):
+        modeldata = {}
         """Count number of documents containing feature and total count of each label"""
         """labelcount totals instances for each label"""
         labelcount = defaultdict(lambda: 0)
@@ -22,26 +25,35 @@ class NaiveBayes(Classifier):
         featurecount = defaultdict(lambda: defaultdict(lambda: 0))
         instancecount = 0
         for instance in instances:
+            labelcount[instance.label] += 1
             for feature in instance.features():
-                labelcount[instance.label] += 1
                 featurecount[instance.label][feature] += 1
         """from feature counts, compute class conditional probability estimates as log"""
         for label in labelcount.keys():
-        	model[label] = {}
-        	for feature in featurecount.keys():
-        		model[label][feature] = math.log( 
-        			(featurecount[feature] + smoothing_factor) / 
-        			(labelcount[label] + smoothing_factor*2) )
+            modeldata[label] = defaultdict(lambda: smoothing_factor/(2*smoothing_factor))
+            for feature in featurecount.keys():
+                modeldata[label][feature] = math.log( 
+                    (featurecount[label][feature] + smoothing_factor) / 
+                    (labelcount[label] + smoothing_factor*2) )
         """compute priors from label counts"""
         instancecount = sum(labelcount.values())
         for label in labelcount.keys():
-        	model[label]['*PRIOR*'] = math.log(labelcount[label] / instancecount)
+            # modeldata[label]['*PRIOR*'] = math.log(labelcount[label] / instancecount)
+            modeldata[label]['*PRIOR*'] = math.log(.5)
 
-
+        self.set_model(modeldata)
 
     def classify(self, instance):
         """Classify an instance using the log probabilities computed during training."""
+        labelsum = {}
+        """initialize with priors"""
+        for label in self.model.keys():
+            labelsum[label] = self.model[label]['*PRIOR*']
+
         for feature in instance.features():
             """sum (log) probabilities in each label and return highest"""
+            for label in self.model.keys():
+                labelsum[label] += self.model[label][feature]
 
-            for label in 
+        """return label with largest sum"""
+        return max(labelsum, key=lambda k: labelsum[k])
